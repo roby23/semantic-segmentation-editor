d04736aa07692334ead92aa7e4589825aa4de65b
Roberto <rbattisti@fbk.eu>
2020-10-23 10:52:33 +0200

    Removed original sets of classes. Remember to reset meteor mongo SseSamples collection.

--- server/config.js
+++ server/config.js
@@ -6,42 +6,34 @@ import download from "download";
 
 const configurationFile = {};
 const defaultClasses = [{
-    "name": "33 Classes", "objects": [
-        {"label": "VOID", "color": "#CFCFCF"},
-        {"label": "Class 1"},
-        {"label": "Class 2"},
-        {"label": "Class 3"},
-        {"label": "Class 4"},
-        {"label": "Class 5"},
-        {"label": "Class 6"},
-        {"label": "Class 7"},
-        {"label": "Class 8"},
-        {"label": "Class 9"},
-        {"label": "Class 10"},
-        {"label": "Class 11"},
-        {"label": "Class 12"},
-        {"label": "Class 13"},
-        {"label": "Class 14"},
-        {"label": "Class 15"},
-        {"label": "Class 16"},
-        {"label": "Class 17"},
-        {"label": "Class 18"},
-        {"label": "Class 19"},
-        {"label": "Class 20"},
-        {"label": "Class 21"},
-        {"label": "Class 22"},
-        {"label": "Class 23"},
-        {"label": "Class 24"},
-        {"label": "Class 25"},
-        {"label": "Class 26"},
-        {"label": "Class 27"},
-        {"label": "Class 28"},
-        {"label": "Class 29"},
-        {"label": "Class 30"},
-        {"label": "Class 31"},
-        {"label": "Class 32"}
+    "name": "Materiali",
+    "objects": [
+      {
+        "label": "Nessuno", 
+        "color": "#FFFFFF"          
+      },
+      {
+        "label": "Lapidei naturali", 
+        "color": "#FF8800"
+      },
+      {
+        "label": "Aggregati artificiali",
+        "color": "#CFCFCF"
+      },
+      {
+        "label": "Organici naturali",
+        "color": "#799024"
+      },
+      {
+        "label": "Metalli",
+        "color": "#FAFFF5"
+      },
+      {
+        "label": "Materiali sintetici",
+        "color": "#9E9BFF"
+      }
     ]
-}];
+  }];
 const defaultDamages = [{
     "name": "Danni", "objects": [
         {"label": "Nessuno", "color": "#CFCFCF"},
